336f7b60edb5b7d0ec05083961f8bd7c28bc5de0
D Hiatt <183925276+TimidTiger44@users.noreply.github.com>
2024-12-23 10:17:16 -0800

    add text to display
    change background because display background
    changes when size of display changes.

--- calculator.js
+++ calculator.js
@@ -1,4 +1,22 @@
-const add = function(a,b) => a+b;
-const subtract = function (a,b) =>a-b;
-const multiply = function(a,b)=> a*b;
-const divide = function(a,b) => a/b;
+const add = (a,b) => a+b;
+const subtract = (a,b) =>a-b;
+const multiply = (a,b)=> a*b;
+const divide = (a,b) => a/b;
+
+let firstNumber;
+let secontNumber;
+let operator;
+// console.log(add(1,2));
+// console.log(subtract(2,1));
+// console.log(multiply(1,2));
+// console.log(divide(2,1));
+function operate(operator,num1,num2){
+    if (operator === "+") {
+        return add(num1,num2)
+    }else if (operator === "-") {
+        return subtract(num1,num2)
+    }else if (operator === "*") {
+        return multiply(num1,num2)
+    }else if (operator === "/") {
+        return divide(num1,num2)}
+}
