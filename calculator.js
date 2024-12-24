2e7081dd55fad781b0c7853c1bde60c27f9a57e8
D Hiatt <183925276+TimidTiger44@users.noreply.github.com>
2024-12-24 06:37:39 -0800

    add functionality to clear button
    Add js so that display clears when button is pushed
    Also, set min height to display area so that it will
    not shrink when display is cleared.

--- calculator.js
+++ calculator.js
@@ -2,8 +2,21 @@ const add = (a,b) => a+b;
 const subtract = (a,b) =>a-b;
 const multiply = (a,b)=> a*b;
 const divide = (a,b) => a/b;
+const display = document.querySelector(".display")
+const calcButtons = document.querySelectorAll(".button")
+const clearButton = document.querySelector("#clear")
 
+function clearDisplay(){
+    display.textContent = ""
+}
 
+clearButton.addEventListener("click", () => {
+    clearDisplay();}
+)
+
+function addToDisplay(number){
+    
+}
 function operate(operator,num1,num2){
     if (operator === "+") {
         return add(num1,num2)
@@ -14,3 +27,4 @@ function operate(operator,num1,num2){
     }else if (operator === "/") {
         return divide(num1,num2)}
 }
+
