63767a8716a88926a5a7d1a652c00357b13ed8bc
D Hiatt <183925276+TimidTiger44@users.noreply.github.com>
2024-12-24 06:18:22 -0800

    refine growing/shrinking of buttons
    Adjust CSS to prevent buttons from growing too
    large and going outside the button container.

--- calculator.js
+++ calculator.js
@@ -3,13 +3,7 @@ const subtract = (a,b) =>a-b;
 const multiply = (a,b)=> a*b;
 const divide = (a,b) => a/b;
 
-let firstNumber;
-let secontNumber;
-let operator;
-// console.log(add(1,2));
-// console.log(subtract(2,1));
-// console.log(multiply(1,2));
-// console.log(divide(2,1));
+
 function operate(operator,num1,num2){
     if (operator === "+") {
         return add(num1,num2)
