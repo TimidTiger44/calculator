3f41a77b5f2fd1d0b2be8e3017acbb05fb185227
D Hiatt <183925276+TimidTiger44@users.noreply.github.com>
2024-12-25 14:57:14 -0800

    Add display populating properties to numbers
    add event listeners and fix classes so that
    display populates with numbers when pressed.

--- calculator.js
+++ calculator.js
@@ -2,29 +2,45 @@ const add = (a,b) => a+b;
 const subtract = (a,b) =>a-b;
 const multiply = (a,b)=> a*b;
 const divide = (a,b) => a/b;
-let total = 0
+
+let flagForDisplayClear = false;
+const num1 = 0;
+const num2 = 0;
+const operators = ["+","-","x","/"]
+
 const display = document.querySelector(".display")
 const numberButtons = document.querySelectorAll(".number")
 const clearButton = document.querySelector("#clear")
+const equals = document.querySelector("#equals")
+const operatorButtons = document.querySelectorAll(".operator")
 
 function clearDisplay(){
-    display.textContent = ""
+    display.textContent = "";
 }
 
 clearButton.addEventListener("click", () => {
-    clearDisplay();}
+    clearDisplay();
+    flagForDisplayClear = false;}
 )
 
 numberButtons.forEach(button => {
     button.addEventListener("click",()=> {
-        total += parseInt(button.textContent)
+        if (flagForDisplayClear) {
+            display.textContent= ""
+        }
+        display.textContent += button.textContent
     }
     )    
 })
 
-function addToDisplay(number){
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        num1 = parseInt(display.textContent);
+        flagForDisplayClear = true
+
+    })
+})
     
-}
 function operate(operator,num1,num2){
     if (operator === "+") {
         return add(num1,num2)
