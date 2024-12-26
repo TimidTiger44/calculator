1ff06c23263fdee8427ccb114bea5b7175d0e1c5
D Hiatt <183925276+TimidTiger44@users.noreply.github.com>
2024-12-25 19:36:56 -0800

    allow for multiple operator commands
    put in another flag so that it will continue to
    calculate the number if another operator is chosen
    before equals is.
    
    Add an array of objects with each choice of
    operator.

--- calculator.js
+++ calculator.js
@@ -4,9 +4,28 @@ const multiply = (a,b)=> a*b;
 const divide = (a,b) => a/b;
 
 let flagForDisplayClear = false;
+let operatorPresent = false;
 let num1 = 0;
 let num2 = 0;
 let operator = ""
+let operators= [
+    {
+        operator:"+", 
+        id:"plus",
+    },
+    {
+        operator:"-", 
+        id:"minus",
+    },
+    {
+        operator:"/", 
+        id:"divide",
+    },
+    {
+        operator:"*", 
+        id:"multiply",
+    }
+];
 
 const display = document.querySelector(".display")
 const numberButtons = document.querySelectorAll(".number")
@@ -16,32 +35,13 @@ const operatorButtons = document.querySelectorAll(".operator")
 
 function clearDisplay(){
     display.textContent = "";
+    flagForDisplayClear = false;
+    operatorPresent = false;
+    num1 = 0;
+    num2 = 0;
+    operator= "";
 }
 
-clearButton.addEventListener("click", () => {
-    clearDisplay();
-    flagForDisplayClear = false;}
-)
-
-numberButtons.forEach(button => {
-    button.addEventListener("click",()=> {
-        if (flagForDisplayClear) {
-            display.textContent= ""
-        }
-        display.textContent += button.textContent
-    }
-    )    
-})
-
-operatorButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        num1 = parseInt(display.textContent);
-        console.log(num1)
-        flagForDisplayClear = true
-
-    })
-})
-    
 function operate(operator,num1,num2){
     if (operator === "+") {
         return add(num1,num2)
@@ -52,4 +52,52 @@ function operate(operator,num1,num2){
     }else if (operator === "/") {
         return divide(num1,num2)}
 }
+
+equals.addEventListener("click", () => {
+    display.textContent = operate(operator,num1,num2);
+    num1 = 0;
+    num2 = 0;
+    operator = "";
+    flagForDisplayClear= false;
+    operatorPresent = false;
+
+})
+
+clearButton.addEventListener("click", () => {
+    clearDisplay();}
+)
+
+numberButtons.forEach(button => {
+    button.addEventListener("click",()=> {
+        if (flagForDisplayClear) {
+            display.textContent= ""
+            flagForDisplayClear = false
+        }
+        display.textContent += button.textContent
+    }
+    )    
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        if (operatorPresent) {
+            num2 = parseInt(display.textContent)
+            num1 = operate(operator,num1,num2)
+            display.textContent = num1
+            // let id = event.target.getAttribute('id')
+            let operatorObj  = operators.find(obj => obj.id === button.id)
+            // console.log(operatorObj)
+            operator = operatorObj.operator
+            // console.log(operator)
+        } else {
+
+        num1 = parseInt(display.textContent);
+        console.log(num1);
+        flagForDisplayClear = true;
+        operatorPresent = true;
+
+}})
+})
+    
+
 
