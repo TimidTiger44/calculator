b8955007397260e0c9e1b84d2bb6098b9158497b
D Hiatt <183925276+TimidTiger44@users.noreply.github.com>
2024-12-28 11:22:45 -0800

    Disable equals button if already pressed.
    create flag so that equals use will not be able to
    repeat functionality if accidentally pressed twice.

--- calculator.js
+++ calculator.js
@@ -1,10 +1,11 @@
 const add = (a,b) => a+b;
 const subtract = (a,b) =>a-b;
 const multiply = (a,b)=> a*b;
-const divide = (a,b) => a/b;
+const divide = (a,b) => b===0 ? "No zero division!":a/b;
 
 let flagForDisplayClear = false;
 let operatorPresent = false;
+let equalDisable = false;
 let num1 = 0;
 let num2 = 0;
 let operator = ""
@@ -36,7 +37,7 @@ const operatorButtons = document.querySelectorAll(".operator")
 function formatResult (result) {
     let digits = result.toString().length;
     if (digits>12) {
-        return result.toPrecision(12);
+        return result.toPrecision(11);
     }else { return result
 }}
 
@@ -44,6 +45,7 @@ function clearDisplay(){
     display.textContent = "";
     flagForDisplayClear = false;
     operatorPresent = false;
+    equalDisable = false;
     num1 = 0;
     num2 = 0;
     operator= "";
@@ -61,15 +63,20 @@ function operate(operator,num1,num2){
 }}
 
 equals.addEventListener("click", () => {
+    if(equalDisable) return;
+        
     num2 = parseInt(display.textContent)
     console.log(num2)
-    display.textContent = operate(operator,num1,num2);
+    if (num2 === 0) {
+        display.textContent = "No 0 division"
+    }else {
+    display.textContent = operate(operator,num1,num2)};
     num1 = 0;
     num2 = 0;
     operator = "";
     flagForDisplayClear= true;
     operatorPresent = false;
-
+    equalDisable = true;
 })
 
 clearButton.addEventListener("click", () => {
@@ -80,6 +87,7 @@ numberButtons.forEach(button => {
     button.addEventListener("click",()=> {
         if (flagForDisplayClear) {
             display.textContent= ""
+            equalDisable = false;
             flagForDisplayClear = false;
         }
         display.textContent += button.textContent
